c93b519f2016036db709f90893436ced678c36eb
Tad Heigel <theig072@gmail.com>
2014-05-07 17:32:49 -0400

    added in a few objects to work with

---
+++ main.js
@@ -0,0 +1,97 @@
+var tad = new Object();
+tad.name = "Thaddeous";
+tad.gender = "Male";
+tad.age = 26;
+
+var shannon = new Object();
+shannon.name = "Shannon";
+shannon.gender = "Female";
+shannon.age = 25;
+
+shannon.weight = {
+    pounds: 100,
+    ounzes: 0, 
+  }
+
+shannon.height = {
+    feet: 5,
+    inches: 6,
+  }
+
+shannon.birthday = {
+    Month: "June",
+    Day: 18,
+    Year: 1988,
+  }
+
+var duke = new Object();
+duke.petname = "Duke";
+duke.nickname = "Dukem Nukem";
+duke.breed = "Rottweiler";
+duke.color = "Dark Red";
+
+duke.birthday = {
+    Month: "January",
+    Day: 25,
+    Year: 2005,
+  }
+
+console.log(tad);
+console.log(shannon);
+console.log(duke);
+
+
+
+
+
+// var familyMember = {
+//   name: "Hannah",
+//   nickname: "Bean",
+//   relation: "sister",
+
+// console.log("My name is", name, ".");
+// console.log("I am", age, "yrs old.");
+// console.log("My weight is", weight.pounds, weight.pounds.ounces ".");
+// console.log("My favorite pet:", pet.petname, ".");
+// console.log("I am:", gender, ".");
+// console.log("My pet's nickname is", pet.petsnickname, ".");
+// console.log("I was born on", birthday, ".");
+// console.log("My dog was born in the year", pet.petsbirthday, ".");
+
+
+// // // Functions
+// // function dogAge (tad, joe, whitney, lance, luke, william) {
+// // return tad - joe - whitney - lance - luke + william;
+// // };
+
+// // console.log(dogAge(dog.dYear, dog.bYear));
+
+
+
+
+
+
+// // Objects
+
+// // // Variables
+
+// // Strings
+
+// // Comparison Operators
+
+// // Methods
+
+// // Data Type - Undefined
+
+// // Data Type - Null
+
+// // Booean Values
+
+// // If/Else Statements
+
+// // Functions
+
+// // Data Structures - Objects
+
+// // Data Structures - Arrays
+
